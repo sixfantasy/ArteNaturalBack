6352d53cf34c6f8899681951677e9cd6ac820271
KevinMS98 <kevinmallque3010@gmail.com>
2025-10-15 17:09:07 +0200

    public images 2

--- src/main/java/com/artenatural/Back/config/SecurityConfig.java
+++ src/main/java/com/artenatural/Back/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login",
                                 "/auth/register","/Images/**",
-                                "/uploads/list/all").permitAll() // Permit access to specific endpoints
+                                "/uploads/list/all"
+                        ).permitAll() // Permit access to specific endpoints
                         .requestMatchers("/uploads/upload").hasAuthority("ARTIST")
                         .anyRequest().authenticated() // All other requests require authentication
                 )
