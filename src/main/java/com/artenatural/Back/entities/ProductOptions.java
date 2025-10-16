88740d0e2723e2c27aa658df0590aca7f6c8b388
Jaume <mortejauroldan@gmail.com>
2025-10-16 19:13:00 +0200

    Arreglo referencia circular en PurchaseItem

--- src/main/java/com/artenatural/Back/entities/ProductOptions.java
+++ src/main/java/com/artenatural/Back/entities/ProductOptions.java
@@ -1,5 +1,6 @@
 package com.artenatural.Back.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class ProductOptions {
     private String category;
     private String option;
     private double price;
+    @JsonIgnore
     @ManyToOne
     private Product product;
 }
