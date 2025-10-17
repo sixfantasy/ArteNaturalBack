64eaa5806089d22d9d68bce32eefd0b4ea21d81d
Alex diaz <mrplanning2@gmail.com>
2025-10-17 15:59:22 +0200

    CORS configuration added

--- src/main/java/com/artenatural/Back/config/SecurityConfig.java
+++ src/main/java/com/artenatural/Back/config/SecurityConfig.java
@@ -16,13 +16,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://artenaturalback-production.up.railway.app")); // Cambia por tu URL
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Value("${app.local-domain-front}")
     private String localDomainFront;
 
@@ -43,7 +59,7 @@ public class SecurityConfig {
         http
                 // Disable CSRF since we are using JWT (stateless)
                 .csrf(csrf -> csrf.disable())
-                .cors(withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // Define authorization rules
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login",
